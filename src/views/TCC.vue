e51986d84a1bcf9b8e8cccc4d35d8a0bef30234f
RafaelOliveiraP <rop.msg@gmail.com>
2024-06-13 02:37:09 -0300

    Foi incluída a seleção de semestres, o .ts do TCC 2024_1, e seus filtros

--- src/views/TCC.vue
+++ src/views/TCC.vue
@@ -12,42 +12,97 @@
       <h4 class="text-center">APRESENTAÇÕES DE TCC</h4>
       <br>
       <br>
-      
 
-      <div class="filtro">
-        <div class="text-center">
-          <form>
-            <div class="form">
-              <div class="radioButton">
-                <input v-on:click="filtrarTCC1()" type="radio" id="tcc1" name="tcc" value="tcc1">
-                <label for="tcc1" class="label-radioButton"><b>TCC I</b></label>
-              </div>
-    
-              <div class="radioButton">
-                <input type="radio" v-on:click="filtrarTCC2()" id="tcc2" name="tcc" value="tcc2" checked>
-                <label for="tcc2" class="label-radioButton"><b>TCC II</b></label>
-              </div>
-            </div>
-          </form>
+      <div class="text-center">
+        <div class="dropdown-container">
+          <label for="semester">
+            <h4>Selecione o semestre: </h4>
+            </label>
+          <select id="semester" v-model="selectedSemester" @change="handleChange">
+            <option value="2024/1">2024/1</option>
+            <option value="2023/2">2023/2</option>
+          </select>
         </div>
-        <div class="button-container">
-          <button class="button" v-on:click="clearFilter">Todas as datas</button>
-          <button class="button" v-on:click="filterPalestras">11/12</button>
-          <button class="button" v-on:click="filterPalestras">12/12</button>
-          <button class="button" v-on:click="filterPalestras">13/12</button>
-          <button class="button" v-on:click="filterPalestras">14/12</button>
-          <button class="button" v-on:click="filterPalestras">15/12</button>
-          <button class="button" v-on:click="filterPalestras">16/12</button>          
-        </div>        
       </div>
-      <br>
-      <br>
-      <div class="courses-list">
-        <div v-for="(props, index) in filtered_tccs" :key="index">
-          <!--<h3 class="section-course">{{ props.curso }}</h3> -->
-          <div v-for="(tcc, index) in props.tccs" :key="index">
-            <TccSection :tcc="tcc"></TccSection>
-            <hr />
+
+      <div v-if="selectedSemester === '2023/2'" class="year-month" id="2023_2">
+        <!-- Content for 2023/2 -->
+          <div class="filtro">
+          <div class="text-center">
+            <form>
+              <div class="form">
+                <div class="radioButton">
+                  <input v-on:click="filtrarTCC1()" type="radio" id="tcc1" name="tcc" value="tcc1">
+                  <label for="tcc1" class="label-radioButton"><b>TCC I</b></label>
+                </div>
+      
+                <div class="radioButton">
+                  <input type="radio" v-on:click="filtrarTCC2()" id="tcc2" name="tcc" value="tcc2" checked>
+                  <label for="tcc2" class="label-radioButton"><b>TCC II</b></label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="button-container">
+            <button class="button" v-on:click="clearFilter">Todas as datas</button>
+            <button class="button" v-on:click="filterPalestras">11/12</button>
+            <button class="button" v-on:click="filterPalestras">12/12</button>
+            <button class="button" v-on:click="filterPalestras">13/12</button>
+            <button class="button" v-on:click="filterPalestras">14/12</button>
+            <button class="button" v-on:click="filterPalestras">15/12</button>
+            <button class="button" v-on:click="filterPalestras">16/12</button>          
+          </div>        
+        </div>
+        <br>
+        <br>
+        <div class="courses-list">
+          <div v-for="(props, index) in filtered_tccs" :key="index">
+            <!--<h3 class="section-course">{{ props.curso }}</h3> -->
+            <div v-for="(tcc, index) in props.tccs" :key="index">
+              <TccSection :tcc="tcc"></TccSection>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-if="selectedSemester === '2024/1'" class="year-month" id="2024_1">
+        <!-- Content for 2024/1 -->
+          <div class="filtro">
+          <div class="text-center">
+            <form>
+              <div class="form">
+                <div class="radioButton">
+                  <input v-on:click="filtrarTCC1()" type="radio" id="tcc1" name="tcc" value="tcc1">
+                  <label for="tcc1" class="label-radioButton"><b>TCC I</b></label>
+                </div>
+      
+                <div class="radioButton">
+                  <input type="radio" v-on:click="filtrarTCC2()" id="tcc2" name="tcc" value="tcc2" checked>
+                  <label for="tcc2" class="label-radioButton"><b>TCC II</b></label>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="button-container">
+            <button class="button" v-on:click="clearFilter">Todas as datas</button>
+            <button class="button" v-on:click="filterPalestras">17/06</button>
+            <button class="button" v-on:click="filterPalestras">18/06</button>
+            <button class="button" v-on:click="filterPalestras">19/06</button>
+            <button class="button" v-on:click="filterPalestras">20/06</button>
+            <button class="button" v-on:click="filterPalestras">21/06</button>
+            <button class="button" v-on:click="filterPalestras">22/06</button>          
+          </div>        
+        </div>
+        <br>
+        <br>
+        <div class="courses-list">
+          <div v-for="(props, index) in filtered_tccs" :key="index">
+            <!--<h3 class="section-course">{{ props.curso }}</h3> -->
+            <div v-for="(tcc, index) in props.tccs" :key="index">
+              <TccSection :tcc="tcc"></TccSection>
+              <hr />
+            </div>
           </div>
         </div>
       </div>
@@ -57,125 +112,119 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
-import PhotoHeader from '../components/organization/PhotoHeader.vue'
-import Main from '../components/organization/Main.vue'
-import TccSection from '../components/tccSection/index.vue'
+import { Component, Vue } from 'vue-property-decorator';
+import Main from '../components/organization/Main.vue';
+import TccSection from '../components/tccSection/index.vue';
 
-import { apresentacoes_TCC } from '@/storage/programacao/TCCs'
-
-
+// Import data files for both semesters
+import { apresentacoes_TCC as apresentacoes_TCC_2023_2 } from '@/storage/programacao/TCCs_2023_2';
+import { apresentacoes_TCC as apresentacoes_TCC_2024_1 } from '@/storage/programacao/TCCs_2024_1';
 
 @Component({
   components: {
-    PhotoHeader,
     Main,
     TccSection
   }
 })
 export default class TCC extends Vue {
+  private selectedSemester: string = '2024/1';
+  private tipo_TCC: number = 2;
+  private ultima_data: string | null = null;
+  private all_tccs: any[] = [];
+  private filtered_tccs: any[] = [];
 
-  private tipo_TCC
-  private ultima_data
-  
+  mounted() {
+    this.loadData();
+  }
 
-  private all_tccs
+  handleChange() {
+    this.loadData();
+  }
 
-  private filtered_tccs
+  loadData() {
+    // Determine which data to use based on selected semester
+    if (this.selectedSemester === '2023/2') {
+      this.all_tccs = apresentacoes_TCC_2023_2;
+    } else if (this.selectedSemester === '2024/1') {
+      this.all_tccs = apresentacoes_TCC_2024_1;
+    }
 
-  private title = 'TCC'
-  private description = 'Trabalho De Conclusão De Curso'
-  private background = 'assets/img/slider/6.jpg'
+    // Apply filters based on tipo_TCC and ultima_data
+    this.applyFilters();
+  }
+
+  applyFilters() {
+    if (this.ultima_data === null) {
+      this.filtered_tccs = this.all_tccs.map(item => ({
+        ...item,
+        tccs: item.tccs.filter(aux => aux.tipoTCC === this.tipo_TCC)
+      }));
+    } else {
+      this.filtered_tccs = this.all_tccs.map(item => ({
+        ...item,
+        tccs: item.tccs.filter(aux => aux.date === this.ultima_data && aux.tipoTCC === this.tipo_TCC)
+      }));
+    }
+  }
 
   filterPalestras(e) {
-    let data = e.target.innerText
+    let data = e.target.innerText;
     this.ultima_data = data;
-
-    this.filtered_tccs = this.all_tccs.map(item => {
-      let tccs = item.tccs.filter(aux => {
-        return (aux.date == data) && (aux.tipoTCC == this.tipo_TCC)
-      })
-      return { ...item, tccs }
-    })
+    this.applyFilters();
   }
 
   clearFilter() {
     this.ultima_data = null;
-    this.filtered_tccs = this.all_tccs
-
-    this.filtered_tccs = this.all_tccs.map(item => {
-      let tccs = item.tccs.filter(aux => {
-        return aux.tipoTCC == this.tipo_TCC
-      })
-      return { ...item, tccs }
-    })
+    this.applyFilters();
   }
 
   filtrarTCC1() {
     this.tipo_TCC = 1;
-
-    if (this.ultima_data === null) {
-      this.filtered_tccs = this.all_tccs.map(item => {
-        let tccs = item.tccs.filter(aux => {
-          return aux.tipoTCC == this.tipo_TCC
-        })
-        return { ...item, tccs }
-      })
-
-      return;
-    } 
-
-    this.filtered_tccs = this.all_tccs.map(item => {
-      let tccs = item.tccs.filter(aux => {
-        return  (aux.date == this.ultima_data) && (aux.tipoTCC == this.tipo_TCC)
-      })
-      return { ...item, tccs }
-    })    
+    this.applyFilters();
   }
 
   filtrarTCC2() {
     this.tipo_TCC = 2;
-
-    if (this.ultima_data === null) {
-      this.filtered_tccs = this.all_tccs.map(item => {
-        let tccs = item.tccs.filter(aux => {
-          return aux.tipoTCC == this.tipo_TCC
-        })
-        return { ...item, tccs }
-      })
-
-      return;
-    } 
-
-    this.filtered_tccs = this.all_tccs.map(item => {
-      let tccs = item.tccs.filter(aux => {
-        return  (aux.date == this.ultima_data) && (aux.tipoTCC == this.tipo_TCC)
-      })
-      return { ...item, tccs }
-    })
+    this.applyFilters();
   }
-
-  constructor() {
-    super()
-
-    this.ultima_data = null
-    this.tipo_TCC = 2
-    this.all_tccs = apresentacoes_TCC
-    this.filtered_tccs = this.all_tccs
-
-    this.filtered_tccs = this.all_tccs.map(item => {
-      let tccs = item.tccs.filter(aux => {
-        return aux.tipoTCC == this.tipo_TCC
-      })
-      return { ...item, tccs }
-    })
-  }
-
-  
 }
 </script>
 
 <style scoped>
+
+ /* General styles for dropdown container */
+ .dropdown-container {
+      display: inline-block;
+      font-family: Arial, sans-serif; /* Adjust font-family as needed */
+      font-size: 14px; /* Adjust font-size as needed */
+      text-align: center; 
+    }
+  .dropdown-container label {
+    margin-right: 10px; /* Adjust as needed */
+  }
+
+    /* Styling for the select element */
+    .dropdown-container select {
+      appearance: none; /* Remove default appearance (e.g., arrow) */
+      padding: 10px 40px 10px 15px; /* Padding for select box */
+      border: 1px solid #ccc; /* Border style */
+      background-color: #fff; /* Background color */
+      color: #333; /* Text color */
+      cursor: pointer; /* Cursor style on hover */
+      border-radius: 4px; /* Optional: Rounded corners */
+      outline: none; /* Remove outline */
+      width: 160px; /* Set width as needed */
+    }
+
+    /* Styling for the dropdown arrow icon (optional) */
+    .dropdown-container::after {
+      content: '\25BC'; /* Unicode character for down arrow */
+      position: absolute;
+      top: 50%;
+      right: 10px;
+      transform: translateY(-50%);
+      pointer-events: none; /* Ensure arrow is not clickable */
+    }
 
 .main {
   padding-top: 50px;
@@ -280,4 +329,5 @@ h4 {
   color: black;
   margin-bottom: 3rem;
 }
+
 </style>
